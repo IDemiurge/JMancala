df0e1d8ded35d0cfbaa5fe784afe8dc59ad6dbc6
IDemiurge <alexanderkkamen@gmail.com>
2023-12-14 22:41:16 +0100

    Added a more realistic logic test with 5 turns

--- src/test/java/mancala/ClassicMancalaTests.java
+++ src/test/java/mancala/ClassicMancalaTests.java
@@ -3,12 +3,15 @@ package mancala;
 import mancala.config.ClassicMancalaConfig;
 import mancala.game.IGameService;
 import mancala.game.logic.state.GameState;
+import mancala.game.utils.MancalaStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by Alexander on 12/11/2023
@@ -63,6 +66,44 @@ public class ClassicMancalaTests {
 
         Assert.isTrue(updatedState.pits()[11] == 0, "Game should have captured stones");
         Assert.isTrue(updatedState.pits()[6] == 6, "Game should have put captured stones into player's store");
+    }
+
+    @Test
+    public void testGame() {
+        int[] pits;
+        GameState state = gameService.host();
+        gameService.join(state);
+
+        state = gameService.makeMove(state, 2);
+
+        pits = new int[]{
+                6, 6, 0, 7, 7, 7, 1, 7, 7, 6, 6, 6, 6, 0
+        };
+        assertEquals(MancalaStringUtils.prettyPits(state.pits()), MancalaStringUtils.prettyPits(pits), "Different pits");
+
+        state = gameService.makeMove(state, 0+7);
+        pits = new int[]{
+                7, 6, 0, 7, 7, 7, 1, 0, 8, 7, 7, 7, 7, 1
+        };
+        assertEquals(MancalaStringUtils.prettyPits(state.pits()), MancalaStringUtils.prettyPits(pits), "Different pits");
+
+        state = gameService.makeMove(state, 3);
+        pits = new int[]{
+                7, 6, 0, 0, 8, 8, 2, 1, 9, 8, 8, 7, 7, 1
+        };
+        assertEquals(MancalaStringUtils.prettyPits(state.pits()), MancalaStringUtils.prettyPits(pits), "Different pits");
+
+        state = gameService.makeMove(state, 1+7);
+        pits = new int[]{
+                8, 7, 1, 1, 8, 8, 2, 1, 0, 9, 9, 8, 8, 2
+        };
+        assertEquals(MancalaStringUtils.prettyPits(state.pits()), MancalaStringUtils.prettyPits(pits), "Different pits");
+
+        state = gameService.makeMove(state, 4);
+        pits = new int[]{
+                8, 7, 1, 1, 0, 9, 3, 2, 1, 10, 10, 9, 9, 2
+        };
+        assertEquals(MancalaStringUtils.prettyPits(state.pits()), MancalaStringUtils.prettyPits(pits), "Different pits");
     }
 
 }
